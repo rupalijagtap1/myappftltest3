31ce3395c04cd9e99c8e57d1ad17fad5cc3170ef
Rupali Jagtap <rupalijagtap@google.com>
2022-11-22 11:47:04 -0800

    Initial commit

---
+++ app/src/androidTest/java/com/example/myappftltest3/ExampleInstrumentedTest.kt
@@ -0,0 +1,40 @@
+package com.example.myappftltest3
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.myappftltest3", appContext.packageName)
+    }
+
+    @Test
+    fun passingTest1(){
+        assert(true)
+    }
+
+    @Test
+    fun passingTest2(){
+        assert(true)
+    }
+
+    @Test
+    fun passingTest3(){
+        assert(true)
+    }
+
+}
